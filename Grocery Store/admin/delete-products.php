bab66705818216b5f49a15a163f9d30abf614bf8
Muhammad-Khizer-Hayat <153726451+Muhammad-Khizer-Hayat@users.noreply.github.com>
2024-04-29 21:12:33 -0700

    Attached Delete file
    and Make The Work on Delete Query

--- Grocery Store/admin/delete-products.php
+++ Grocery Store/admin/delete-products.php
@@ -9,6 +9,8 @@ require_once("config.php");
 
  if($result){
     $sql2 = mysqli_query($con ,"DELETE FROM products WHERE category_id = $id");
+    session_start();
+    $_SESSION['delete_msg']='Delete Successfully..........!';
     
 
     header("Location:products.php");
