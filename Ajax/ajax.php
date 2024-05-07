85f50cb5c35364cdb5b339bdc619473446336cf9
Muhammad-Khizer-Hayat <153726451+Muhammad-Khizer-Hayat@users.noreply.github.com>
2024-05-06 18:40:29 -0700

    work on button
    works on grocery store file

--- Ajax/ajax.php
+++ Ajax/ajax.php
@@ -1,16 +1,15 @@
 <?php
 require_once "./config.php";
 
-$sql = "SELECT * FROM fruites";
+$sql = "SELECT * FROM users";
 $result = mysqli_query($con, $sql);
 
 $html = "<table class='table table-striped'>";
 $html .= "<thead>";
 $html .= "<tr>";
 $html .= "<th>ID</th>";
-$html .= "<th>Name</th>";
-$html .= "<th>Description</th>";
-$html .= "<th>Price</th>";
+$html .= "<th>First Name</th>";
+$html .= "<th>Last Name</th>";
 $html .= "<th>Actions</th>";
 $html .= "</tr>";
 $html .= "</thead>";
@@ -21,9 +20,8 @@ $html .= "<tbody>";
 while($row = mysqli_fetch_assoc($result)){
     $html .= "<tr>";
     $html .= "<td>$row[id]</td>";
-    $html .= "<td>$row[name]</td>";
-    $html .= "<td>$row[description]</td>";
-    $html .= "<td>$row[price]</td>";
+    $html .= "<td>$row[fname]</td>";
+    $html .= "<td>$row[lname]</td>";
     $html .= "<td><button class='btn btn-warning btn-sm me-2 editBtn' data-id='$row[id]'>Edit</button><button class='btn btn-danger btn-sm'>Delete</button></td>";
     $html .= "</tr>";
 }
--- Ajax/update.php
+++ Ajax/update.php
@@ -3,9 +3,9 @@ require_once "./config.php";
 
 if(isset($_POST['id'])) {
     $id = $_POST['id'];
-    $name = $_POST['name'];
+    $fname = $_POST['fname'];
 
-    $sql = "UPDATE items SET `name`='$name' WHERE id=$id";
+    $sql = "UPDATE users SET `fname`='$fname' WHERE id=$id";
     $result = mysqli_query($con, $sql);
     if ($result) {
         echo "successfully updated";
